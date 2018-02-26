0d7f5bd6f513a5b42dcf510d60487ae3a03d08f0
Basti <bastiangraebener@gmail.com>
2018-02-26 11:25:58 +0000

    initial commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package RegExpEngine
+
+/**
+RegExpEngine
+
+Author: Bastian Graebener - G00340600
+
+Simple regular expression engine in GoLang.
+Project for Graph Theory Module in Year 3 of GMIT Bsc. Software Delevopment.
+
+main.go - Main file
+ */
+
+func main() {
+
+}
